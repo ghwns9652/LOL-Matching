caed2e802e2e9e72937224a890d9b9c661d5daf2
YONGZOO <dracol1004@gmail.com>
2020-05-24 23:06:00 +0900

    clustering-consider position and party size

--- main.py
+++ main.py
@@ -100,7 +100,21 @@ def normal_sorting(candidate, games):
             team_a, team_b = [], []
 
 def clustering(candidate, games):
-  
+    def powerset(s):
+        x = len(s)
+        for i in range(1 << x):
+            yield [s[j] for j in range(x) if (i & (1 << j))]  
+    
+    def makenewparty(party_list):
+        OR_2b = party_list[0].position
+        SUM_2b = party_list[0].position
+        for party in party_list[1:]:
+            OR_2b |= party.position
+        for party in party_list[1:]:
+            SUM_2b += party.position
+        return OR_2b == SUM_2b
+
+
     #reconfigure party queue for clustering by gentime,avg_mmr
     candidate_feature_arr = []
     for party in candidate:
@@ -158,53 +172,214 @@ def clustering(candidate, games):
         full_team = []
         # 1 or 2 or 3
         try_team = [[] for _ in range(0b11111+1)] #multiqueue_poistion[0b00001] ~ multiqueue_poistion[0b11111]
+        try_team_1 = []
+        try_team_2 = []
+        try_team_3 = []
         try_team_num = 0
+        make_pair = False
         # 4
         not_team = []
+        
         for party in same_labeled_group:
-        # O(same_labeled_group)
-            num = len(party.party_size)
+            #pairs are made 
+            if party not in multiqueue_position[party.position]:
+                continue
+
+            # O(same_labeled_group)
+            num = party.party_size
             idx = 0b11111^party.position
             # [5]
             if party.position == 0b11111:
                 full_team.append(party)
+                multiqueue_position[party.position].remove(party) #delete
             
             # [1,4] [2,3] pair exists
             elif len(multiqueue_position[idx])>0:
                 full_team.append([party,multiqueue_position[idx][0]])
-                deleted_party.append(party) #deleted outer loop
-                deleted_party.append(multiqueue_position[idx][0]) #deleted outer loop
+                deleted_party.append(party) #delete in candidate at outer loop
+                deleted_party.append(multiqueue_position[idx][0]) #delete in candidate at outer loop
                 del multiqueue_position[idx][0] #delete
-            
+                multiqueue_position[party.position].remove(party) #delete
+
             else: # pair doesnt exists
                 if num==4:
                     not_team.append(party)
+                elif num == 3:
+                    try_team_3.append(party)
+                    try_team[party.position].append(party)
+                    try_team_num += 1
+                elif num == 2:
+                    try_team_2.append(party)
+                    try_team[party.position].append(party)
+                    try_team_num += 1
+                elif num == 1:
+                    try_team_1.append(party)
+                    try_team[party.position].append(party)
+                    try_team_num += 1
                 else:
                     try_team[party.position].append(party)
                     try_team_num += 1
 
+            '''
             # make new pair
             # they are 1 or 2 or 3 and they has no exact partner
             if try_team_num >= 2: # at least one pair can be made
-                pick = []           # O(try_team_num)
+                
+                pick = []         # O(try_team_num)
                 for same_pos_party_list in try_team: 
                     if len(same_pos_party_list) != 0:
-                        for party in same_pos_party_list:
-                            pick.append(party)
-                
-                for i in range(len(pick)):
-                    for j in range(len(pick[i:])):
-                        #make pairs
-                        if pick[i].position & pick[i+j].position == 0b00000:
+                        for same_pos_party in same_pos_party_list:
+                            pick.append(same_pos_party)
+                print("len of pick: ", len(pick))
+                for set_elemlist in powerset(pick):
+                    #check already paired
+                    deleted_exist = False
+                    for set_elem in set_elemlist:
+                        if set_elem not in multiqueue_position[set_elem.position]:
+                            deleted_exist = True   
+
+                    if deleted_exist == True:
+                        continue
+
+                    #none of set_elemlist is not removed
+                    if len(set_elemlist) >= 2:
+                        OR_2b = set_elemlist[0].position
+                        SUM_2b = set_elemlist[0].position
+                        for set_elem in set_elemlist[1:]:
+                            OR_2b |= set_elem.position
+                        for set_elem in set_elemlist[1:]:
+                            SUM_2b += set_elem.position
+
+                        if SUM_2b == OR_2b:
                             #check
-                            idx = pick[i].position | pick[i+j].position
+                            idx = 0b11111^(OR_2b)
                             if len(multiqueue_position[idx])>0:
-                                full_team.append([pick[i],pick[i+j],multiqueue_position[idx][0]])
-                                deleted_party.append(pick[i])   #deleted outer loop
-                                deleted_party.append(pick[i+j]) #deleted outer loop
+                                makepair = True
+                                full_team.append([set_elemlist,multiqueue_position[idx][0]])
+                                try_team_num -= len(set_elemlist)
+                                for s in set_elemlist:
+                                    deleted_party.append(s)
+                                    multiqueue_position[s.position].remove(s)
+                                    try_team[s.position].remove(s)
                                 del multiqueue_position[idx][0] #delete
-                        else:
-                            pass
+            '''
+            #### (1,2)
+            if len(try_team_1) >= 1 and len(try_team_2) >= 1:
+                try_team_1.sort(key=lambda x: x.gentime,reverse=True)
+                try_team_2.sort(key=lambda x: x.gentime,reverse=True)
+                for solo in try_team_1:
+                    if solo not in multiqueue_position[solo.position]:
+                        continue
+                    OR_2b = solo.position
+                    SUM_2b = solo.position
+
+                    for duo in try_team_2:
+                        if duo not in multiqueue_position[duo.position]:
+                            continue
+                        OR_2b |= duo.position
+                    for duo in try_team_2:
+                        if duo not in multiqueue_position[duo.position]:
+                            continue
+                        SUM_2b |= duo.position
+
+                    if OR_2b == SUM_2b:
+                        #check
+                        idx = 0b11111^(OR_2b)
+                        if len(multiqueue_position[idx])>0:
+                            makepair = True
+                            full_team.append([solo,duo,multiqueue_position[idx][0]])
+                            try_team_num -= 2
+                            deleted_party.append(solo)
+                            deleted_party.append(duo)
+                            try_team_1.remove(solo)
+                            try_team_2.remove(duo)
+                            del multiqueue_position[idx][0] #delete
+            
+            #(1,1,1,1)
+            if len(try_team_1) >= 4 :
+                for solo_1 in try_team_1:
+                    if solo_1 not in multiqueue_position[solo_1.position]:
+                        continue
+                    for solo_2 in try_team_1:
+                        if solo_2 not in multiqueue_position[solo_2.position]:
+                            continue
+                        for solo_3 in try_team_1:
+                            if solo_3 not in multiqueue_position[solo_3.position]:
+                                continue
+                            for solo_4 in try_team_1:
+                                if solo_4 not in multiqueue_position[solo_4.position]:
+                                    continue
+                                if makenewparty([solo_1,solo_2,solo_3,solo_4]) == True:
+                                    idx = 0b11111^(solo_1.position | solo_2.position | solo_3.position | solo_4.position)
+                                    if len(multiqueue_position[idx])>0:    
+                                        full_team.append([solo_1,solo_2,solo_3,solo_4,multiqueue_position[idx][0]])
+                                        try_team_num -= 4
+                                        try_team_1.remove(solo_1)
+                                        try_team_1.remove(solo_2)
+                                        try_team_1.remove(solo_3)
+                                        try_team_1.remove(solo_4)
+                                        deleted_party.append(solo_1)
+                                        deleted_party.append(solo_2)
+                                        deleted_party.append(solo_3)
+                                        deleted_party.append(solo_4)
+                                        multiqueue_position[solo_1.position].remove(solo_1)
+                                        multiqueue_position[solo_2.position].remove(solo_2)
+                                        multiqueue_position[solo_3.position].remove(solo_3)
+                                        multiqueue_position[solo_4.position].remove(solo_4)
+                                        del multiqueue_position[idx][0] #delete
+            #(1,1,1)
+            if len(try_team_1) >= 3 :
+                for solo_1 in try_team_1:
+                    if solo_1 not in multiqueue_position[solo_1.position]:
+                                continue
+                    for solo_2 in try_team_1:
+                        if solo_2 not in multiqueue_position[solo_2.position]:
+                                continue
+                        for solo_3 in try_team_1:
+                            if solo_3 not in multiqueue_position[solo_3.position]:
+                                continue
+                            if makenewparty([solo_1,solo_2,solo_3]) == True:
+                                idx = 0b11111^(solo_1.position | solo_2.position | solo_3.position)
+                                if len(multiqueue_position[idx])>0:    
+                                    full_team.append([solo_1,solo_2,solo_3,multiqueue_position[idx][0]])
+                                    try_team_num -= 3
+                                    try_team_1.remove(solo_1)
+                                    try_team_1.remove(solo_2)
+                                    try_team_1.remove(solo_3)
+                                    deleted_party.append(solo_1)
+                                    deleted_party.append(solo_2)
+                                    deleted_party.append(solo_3)
+                                    multiqueue_position[solo_1.position].remove(solo_1)
+                                    multiqueue_position[solo_2.position].remove(solo_2)
+                                    multiqueue_position[solo_3.position].remove(solo_3)
+                                    del multiqueue_position[idx][0] #delete
+            #(1,1)
+            if len(try_team_1) >=2:
+                for solo_1 in try_team_1:
+                    if solo_1 not in multiqueue_position[solo_1.position]:
+                                continue
+                    for solo_2 in try_team_1:
+                        if solo_2 not in multiqueue_position[solo_2.position]:
+                                continue
+                        if makenewparty([solo_1,solo_2]) == True:
+                            idx = 0b11111^(solo_1.position | solo_2.position)
+                            if len(multiqueue_position[idx])>0:    
+                                full_team.append([solo_1,solo_2,multiqueue_position[idx][0]])
+                                try_team_num -= 2
+                                try_team_1.remove(solo_1)
+                                try_team_1.remove(solo_2)
+                                deleted_party.append(solo_1)
+                                deleted_party.append(solo_2)
+                                multiqueue_position[solo_1.position].remove(solo_1)
+                                multiqueue_position[solo_2.position].remove(solo_2)
+                                del multiqueue_position[idx][0] #delete
+  
+                ###
+                     
+            if len(full_team) > 0 and len(full_team)%2 == 0:
+                for i in range(len(full_team)//2):
+                    games.append([full_team[2*i],full_team[2*i+1]])
+        
 
     for party in deleted_party:
         if party in candidate:
@@ -228,9 +403,12 @@ def matchmaking(execution_time):
     global novice
     start_ts = time.time()
     func = "clustering"
+
     
 
     while(time.time() - start_ts < execution_time):
+        print(time.time() - start_ts)
+
         #if(len(novice) >= 10):
         #    make_matches(novice, games)
         if(len(queue) >= search_window):
@@ -242,16 +420,16 @@ def matchmaking(execution_time):
     
 
 def main():
-    t0 = threading.Thread(target=generation, args=(10000, "uniform"))
+    t0 = threading.Thread(target=generation, args=(1000, "uniform"))
     t1 = threading.Thread(target=matchmaking, args=[3])
     t0.start()
     t1.start()
 
-generation(10000,"uniform")
+generation(1000,"uniform")
 matchmaking(3)
-time.sleep(3)
+
 
 print(len(games))
-#print(games[0][0].avg_mmr)
-#print(games[0][0].avg_exp)
-#print(games[0][0].gentime)
+print(games[0][0].avg_mmr)
+print(games[0][0].avg_exp)
+print(games[0][0].gentime)
