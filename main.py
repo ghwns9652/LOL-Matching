033c238301e7339e9da4cc00c3f77d5534dcdd5f
ghwns9652 <ghwns9652@dgist.ac.kr>
2020-05-24 16:55:54 +0900

    multi-queue sorting matchmaking done

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from scipy.stats import skewnorm
 games = []
 queue = []
 novice = []
+mqueue = [[]]*31
 search_window = 200
+insert_cnt = 0
+mmr_diff = 100
 
 class Party:
     def __init__(_self, dist):
@@ -40,10 +43,24 @@ class Player:
             _self.mmr = skewnorm.rvs(5, 1000, 500, 1)#FIXME: don't know this is correct distribution
             _self.exp = skewnorm.rvs(100, 10, 8000, 1)#FIXME: don't know this is correct distribution
 
-def generation(period, dist):
+def make_mq_idx(position):
+    mq_idx = position[0]*1 + position[1]*2 + position[2]*4 + position[3]*8 + position[4]*16
+    return mq_idx - 1
+
+def generation(period, dist, qtype):
     global queue
-    for i in range(1, period):
-        queue.append(Party(dist))
+    global mqueue
+    global insert_cnt
+    
+    if(qtype == "sq"):
+        for i in range(1, period):
+            queue.append(Party(dist))
+    elif(qtype == "mq"):
+        for i in range(1, period):
+            p = Party(dist)
+            mq_idx = make_mq_idx(p.position)
+            mqueue[mq_idx].append(p)
+            insert_cnt += 1
 
 # move unexperience noobies to novice list
 # return length of the list
@@ -61,8 +78,90 @@ def remove_novice(candidate, novice):
 
     return i 
 
-def mq_sorting(candidate, games):
+def find_min_diff(mmr_diff, target_q):
+    if(len(target_q) < 2):
+        return [0, 0]
+
+    min_total_mmr_diff = 4*mmr_diff + target_q[1].avg_mmr - target_q[0].avg_mmr
+    ret = [0, 1]
+    for i in range(1, len(target_q)-1):
+        total_mmr_diff = 4*mmr_diff + target_q[i+1].avg_mmr - target_q[i].avg_mmr
+        if(min_total_mmr_diff > total_mmr_diff):
+            min_total_mmr_diff = total_mmr_diff
+            ret = [i, i+1]
     
+    return ret
+
+def mq_match_5(cur_queue, games):
+    team_a, team_b = [], []
+    pair_len = len(cur_queue)//2
+    for i in range(0, pair_len):
+        team_a.append(cur_queue[2*i])
+        team_b.append(cur_queue[2*i+1])
+        games.append([team_a, team_b])
+    del cur_queue[0:2*pair_len]
+    
+def mq_match_234(queue0, queue1, games):
+    team_a, team_b = [], []
+    min_len = len(queue0) if len(queue0) < len(queue1) else len(queue1)
+    pair_len = min_len//2
+
+    for i in range(0, pair_len):
+        q1_idx = find_min_diff(queue0[2*i].avg_mmr - queue0[2*i+1].avg_mmr, queue1)
+        team_a = [queue0[2*i], queue1[q1_idx[1]]]
+        del queue1[q1_idx[1]]
+        team_b = [queue0[2*i+1], queue1[q1_idx[0]]]
+        del queue1[q1_idx[0]]
+        games.append([team_a, team_b])
+    del queue0[0:2*pair_len]
+
+def mq_match_1(p0, p1, p2, p3, p4, games):
+    team_a, team_b = [], []
+    min_len = min([len(p0), len(p1), len(p2), len(p3), len(p4)])
+    pair_len = min_len//2
+
+    for i in range(0, pair_len):
+        team_a = [p0[2*i], p1[2*i+1], p2[2*i], p3[2*i+1], p4[2*i]]
+        team_b = [p0[2*i+1], p1[2*i], p2[2*i+1], p3[2*i], p4[2*i+1]]
+        games.append([team_a, team_b])
+    
+    del p0[0:2*pair_len], p1[0:2*pair_len], p2[0:2*pair_len], p3[0:2*pair_len], p4[0:2*pair_len]
+
+def pos_inverse(position):
+    ret = [0,0,0,0,0]
+    for i in range(0, 5):
+        ret[i] = 1 - position[i]
+    return ret
+
+def mq_sorting(mq_window):
+    global games
+
+    # match 5-person party
+    mq_match_5(mq_window[make_mq_idx([1]*5)], games)
+
+    # match 4-person party with a person
+    pos0 = [[0,1,1,1,1], [1,0,1,1,1], [1,1,0,1,1], [1,1,1,0,1], [1,1,1,1,0]]
+    pos1 = []
+    for i in pos0:
+        pos1.append(pos_inverse(i))
+    for i in range(0, len(pos0)):
+        mq_match_234(mq_window[make_mq_idx(pos0[i])], mq_window[make_mq_idx(pos1[i])], games)
+
+    # match 3-person party with 2-person party
+    pos0 = [[0,0,1,1,1], [0,1,0,1,1], [0,1,1,0,1], [0,1,1,1,0], [1,0,0,1,1], [1,0,1,0,1], [1,0,1,1,0], [1,1,0,0,1], [1,1,0,1,0], [1,1,1,0,0]]
+    pos1 = []
+    for i in pos0:
+        pos1.append(pos_inverse(i))
+    for i in range(0, len(pos0)):
+        mq_match_234(mq_window[make_mq_idx(pos0[i])], mq_window[make_mq_idx(pos1[i])], games)
+
+    # match 1-person party
+    p0 = mq_window[make_mq_idx([1,0,0,0,0])]
+    p1 = mq_window[make_mq_idx([0,1,0,0,0])]
+    p2 = mq_window[make_mq_idx([0,0,1,0,0])]
+    p3 = mq_window[make_mq_idx([0,0,0,1,0])]
+    p4 = mq_window[make_mq_idx([0,0,0,0,1])]
+    mq_match_1(p0, p1, p2, p3, p4, games)
 
 def normal_sorting(candidate, games):
     mmr_sorted = candidate[0:]
@@ -97,44 +196,51 @@ def clustering(candidiate, games):
 
     '''
 
-def make_matches(func, candidate, games):
-    if(func=="normal_sorting"):
+def ns_matchmaking(queue):
+    if(len(queue) >= search_window):
+        candidate = queue[0:search_window]
+        del queue[0:search_window]
+        #remove_novice(candidate, novice)
         normal_sorting(candidate, games)
-    elif(func=="clustering"):
-        clustering(candidate, games)
-    '''
-    add some functions
+        queue = candidate + queue
 
-    '''
-
+def mq_matchmaking(mqueue, insert_cnt):
+    mqueue_window = []
+    if(insert_cnt >= search_window):
+        for i in range(0, 31):
+            temp = mqueue[i][0:]
+            del mqueue[i][0:]
+            temp.sort(key=lambda x: x.avg_mmr)
+            mqueue_window.append(temp)
+        mq_sorting(mqueue_window)
+        for i in range(0, 31):
+            mqueue[i] = mqueue_window[i] + mqueue[i]
 
 def matchmaking(execution_time):
     global queue
     global games
     global novice
+    global mqueue
+    global insert_cnt
     start_ts = time.time()
-    func="normal_sorting"
+    func="mq_sorting"
 
-    while(time.time() - start_ts < execution_time):
-        #if(len(novice) >= 10):
-        #    make_matches(novice, games)
-        if(len(queue) >= search_window):
-            candidate = queue[0:search_window]
-            del queue[0:search_window]
-            #remove_novice(candidate, novice)
-            make_matches(func, candidate, games)
-            queue = candidate + queue
-    
+    if(func == "normal_sorting"):
+        while(time.time() - start_ts < execution_time):
+            ns_matchmaking(queue)
+    elif(func == "mq_sorting"):
+        while(time.time() - start_ts < execution_time):
+            mq_matchmaking(mqueue, insert_cnt)
 
 def main():
-    t0 = threading.Thread(target=generation, args=(10000, "uniform"))
-    t1 = threading.Thread(target=matchmaking, args=[20])
+    t0 = threading.Thread(target=generation, args=(10000, "uniform", "mq"))
+    t1 = threading.Thread(target=matchmaking, args=[100])
     t0.start()
     t1.start()
 
 
 main()
-time.sleep(20)
+time.sleep(100)
 
 print(len(games))
 print(games[0][0][0].avg_mmr)
